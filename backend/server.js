d64171b2917be4b77b0b5f4f34b78bca120d8c65
Gustavo Fontana <gustavoasf77@gmail.com>
2022-07-17 18:12:25 -0300

    CRUD init

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,42 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+let pessoas = [];
 
 app.get('/', (req, res) => {
-  res.status(200).send('Olá Mundo!');
+  res.status(200).send(pessoas);
+});
+
+app.post('/', (req, res) => {
+  console.log('Corpo:', req.body)
+  pessoas.push(req.body);
+  res.status(201).send(req.body);
+});
+
+app.put('/:id', (req, res) => {
+  let pessoaEncontrada = pessoas.filter((pes) => {
+    return pes.id == req.params.id;
+  });
+  pessoaEncontrada = req.body;
+  res.status(202).send(pessoaEncontrada);
+});
+
+app.delete('/:id', (req, res) => {
+  for (let index = 0; index < pessoas.length; index++) {
+    const pessoa = pessoas[index];
+    if (pessoa.id == req.params.id) {
+      pessoas.splice(index, 1);
+    }
+  }
+  res.status(204).send();
 });
 
 app.listen(3000, () => {
-  console.log('Servidor AesDelivery iniciado na porta 3000.');
+  console.log('Servidor AesDelivery iniciado em http://localhost:3000.');
 });
+
+
